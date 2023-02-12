060ccfd6846cde5981e6c16108fcf547d7477773
MuhammadAzril24 <122261465+MuhammadAzril24@users.noreply.github.com>
2023-02-13 00:28:04 +0800

    getrid template
    aku dah buang semua template, ni ada untuk customer, cami nanti kau update admin punya letak dalam folder admin

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,12 @@
-/*!
-    * Start Bootstrap - Creative v6.0.3 (https://startbootstrap.com/themes/creative)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-    */
-    (function($) {
-  "use strict"; // Start of use strict
+let menu = document.queruSelector("#menu-icon");
+let navbar = document.queruSelector(".navbar");
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+menu.addEventListener("click", function() {
+  navbar.classList.toggle("active");
+});
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+window.onscroll = () => {
+  navbar.classList.remove("active");
+};
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 75
-  });
-
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-scrolled");
-    } else {
-      $("#mainNav").removeClass("navbar-scrolled");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
-
-
-
-})(jQuery); // End of use strict
+$('h3 span:first-child').after("<span class=\"dots\"> </span>");
