499f2e6d1bb7d6cee11754cca898e6e111ba088e
Muhdadamiskdr <adamhaniff7196@gmail.com>
2023-03-02 20:02:10 +0800

    Update menu.php

--- menu.php
+++ menu.php
@@ -14,8 +14,26 @@
       </section>
 
       <section class="container" id="container">
-        <div class="container-menu" id="container-menu" style="width: 50px; height: auto;">
-          <img src="image/Burger.png">
+        <div class="container-menu" id="container-menu" style="width: 250px; height: auto;">
+          <img src="image/tomohawk.png" style="position: absolute; top: 500px; left: 200px;"/>
+        </div>
+      </section>
+
+      <section class="container" id="container">
+        <div class="container-menu" id="container-menu" style="width: 250px; height: auto;">
+          <img src="image/Steak.png" style="position: absolute; top: 200px; left: 200px;"/>
+        </div>
+      </section>
+
+      <section class="container" id="container">
+        <div class="container-menu" id="container-menu" style="width: 250px; height: auto;">
+          <img src="image/set platter.png" style="position: absolute; top: 200px; right: 200px;"/>
+        </div>
+      </section>
+
+            <section class="container" id="container">
+        <div class="container-menu" id="container-menu" style="width: 250px; height: auto;">
+          <img src="image/Burger.png" style="position: absolute; top: 200px; right: 550px;"/>
         </div>
       </section>
 </body>
