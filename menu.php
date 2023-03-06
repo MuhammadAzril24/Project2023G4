c9eaa0ec567bc46a9057f20757187d0c341b59da
Muhdadamiskdr <122278423+Muhdadamiskdr@users.noreply.github.com>
2023-03-06 10:18:46 +0800

    Add files via upload

--- menu.php
+++ menu.php
@@ -13,29 +13,187 @@
       </div>
       </section>
 
-      <section class="container" id="container">
-        <div class="container-menu" id="container-menu" style="width: 250px; height: auto;">
-          <img src="image/tomohawk.png" style="position: absolute; top: 500px; left: 200px;"/>
-        </div>
-      </section>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>TOMOHAWK</title>
+  <style>
+    .container {
+      position: relative;
+      text-align: center;
+    }
+    .header {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      padding: 0px;
+      background-color: rgba(0,0,0,0);
+      box-sizing: border-box;
+      font-size: 24px;
+      font-weight: bold;
+      text-align: center;
+    }
+    img {
+      display: block;
+      margin: 0 auto;
+      width: 250px;
+      height: 185px;
+    }
+    p {
+      text-align: left;
+      font-size: 24px;
+      font-weight: bold;
+      margin-top: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header">TOMOHAWK</div>
+  <img src="image/tomohawk.png" alt="Image">
+  <p></p>
+</body>
+</html>
 
-      <section class="container" id="container">
-        <div class="container-menu" id="container-menu" style="width: 250px; height: auto;">
-          <img src="image/Steak.png" style="position: absolute; top: 200px; left: 200px;"/>
-        </div>
-      </section>
 
-      <section class="container" id="container">
-        <div class="container-menu" id="container-menu" style="width: 250px; height: auto;">
-          <img src="image/set platter.png" style="position: absolute; top: 200px; right: 200px;"/>
-        </div>
-      </section>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>steak</title>
+  <style>
+    .container {
+      position: relative;
+      text-align: center;
+    }
+    .header {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      padding: 0px;
+      background-color: rgba(0,0,0,0);
+      box-sizing: border-box;
+      font-size: 24px;
+      font-weight: bold;
+      text-align: center;
+    }
+    img {
+      display: block;
+      margin: 0 auto;
+      width: 250px;
+      height: 185px;
+    }
+    p {
+      text-align: center;
+      font-size: 24px;
+      font-weight: bold;
+      margin-top: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header">STEAK</div>
+  <img src="image/steak.png" alt="Image">
+  <p></p>
+</body>
+</html>
 
-            <section class="container" id="container">
-        <div class="container-menu" id="container-menu" style="width: 250px; height: auto;">
-          <img src="image/Burger.png" style="position: absolute; top: 200px; right: 550px;"/>
-        </div>
-      </section>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Set Platter</title>
+  <style>
+    .container {
+      position: relative;
+      text-align: center;
+    }
+    .header {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      padding: 0px;
+      background-color: rgba(0,0,0,0);
+      box-sizing: border-box;
+      font-size: 24px;
+      font-weight: bold;
+      text-align: center;
+    }
+    img {
+      display: block;
+      margin: 0 auto;
+      width: 250px;
+      height: 185px;
+    }
+    p {
+      text-align: center;
+      font-size: 24px;
+      font-weight: bold;
+      margin-top: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header">PLATTER</div>
+  <img src="image/set platter.png" alt="Image">
+  <p></p>
+</body>
+</html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Burger</title>
+  <style>
+     .container {
+      position: relative;
+      text-align: center;
+    }
+    .header {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      padding: 0px;
+      background-color: rgba(0,0,0,0);
+      box-sizing: border-box;
+      font-size: 24px;
+      font-weight: bold;
+      text-align: center;
+    }
+    img {
+      display: block;
+      margin: 0 auto;
+      width: 250px;
+      height: 185px;
+    }
+    p {
+      text-align: center;
+      font-size: 24px;
+      font-weight: bold;
+      margin-top: 20px;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <div class="header">BURGER</div>
+  <img src="image/burger.png" alt="Image">
+  <p></p>
+</body>
+</html>
+
+
+
+
+
+
+
 </body>
 </html>
 <!-------------footer------------>
